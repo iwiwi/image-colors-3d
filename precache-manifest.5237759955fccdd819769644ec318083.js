60bedf62ab68f5ae22d71b39c8cdcc19eff736c4
Takuya Akiba <t.akiba.65536@gmail.com>
2020-02-08 17:26:01 +0900

    Updates

---
+++ precache-manifest.5237759955fccdd819769644ec318083.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ddfb8c46dac251f4a7bad7e3c253135",
+    "url": "/image-colors-3d/index.html"
+  },
+  {
+    "revision": "c6207b68ebae9c608fdc",
+    "url": "/image-colors-3d/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "1d590811ec32c74f66c2",
+    "url": "/image-colors-3d/static/css/main.a52a8cff.chunk.css"
+  },
+  {
+    "revision": "c6207b68ebae9c608fdc",
+    "url": "/image-colors-3d/static/js/2.d4369c9f.chunk.js"
+  },
+  {
+    "revision": "92fb7a424882d20924bc39cd1a605d50",
+    "url": "/image-colors-3d/static/js/2.d4369c9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d590811ec32c74f66c2",
+    "url": "/image-colors-3d/static/js/main.2a690c85.chunk.js"
+  },
+  {
+    "revision": "6e925daec60ad2a58dfc",
+    "url": "/image-colors-3d/static/js/runtime-main.f093d29a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b6efeb83e7df34bae35f79502e72f76.js"
+  "/image-colors-3d/precache-manifest.5237759955fccdd819769644ec318083.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/image-colors-3d/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
