92f28463a170564b3165348cceb0e4ba35fda64a
Takuya Akiba <t.akiba.65536@gmail.com>
2020-02-08 11:36:17 +0900

    ようやくCSSの構成が分かってきてbootstrapがそれっぽくなった

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import bsCustomFileInput from 'bs-custom-file-input';
+
 import logo from './logo.svg';
 import './App.css';
 import ImageSelection from './components/ImageSelection'
@@ -6,6 +8,17 @@ import ImagePreview from './components/ImagePreview'
 import HSLPlot from './components/HSLPlot'
 
 type FileBlobOrUrl = File | Blob | string;
+
+const NavBar = () => {
+  // view-source:https://bs-custom-file-input.netlify.com/
+  return <div className="App">
+    <nav className="navbar navbar-expand navbar-dark bg-dark text-white">
+      <div className="container">
+        <h1 className="mb-0 h5 py-1">Image Color 3D</h1>
+      </div>
+    </nav>
+    </div>
+};
 
 interface Props {
 }
@@ -25,6 +38,10 @@ class App extends React.Component<Props, State>  {
     //document.addEventListener('paste', this.handlePaste);
   }
 
+  componentDidMount() {
+    bsCustomFileInput.init()
+  }
+
   handleImageChange(file_blob_or_url: FileBlobOrUrl | null) {
     if (file_blob_or_url === null) {
       return;
@@ -37,19 +54,27 @@ class App extends React.Component<Props, State>  {
 
   render() {
     return (
-      <div className="App">
-        <ImageSelection onChange={this.handleImageChange} />
-        {
-          this.state.image_file_blob_or_url === null ?
-            "" : 
-            <div>
-              <ImagePreview fileBlobOrURL={this.state.image_file_blob_or_url} />
-              <HSLPlot fileBlobOrURL={this.state.image_file_blob_or_url} />
+      <div>
+        <NavBar />
+        <div className="container flex-grow-1 flex-shrink-0 mt-5">
+          <div className="row">
+            <div className="col-sm-12">
+              <ImageSelection onChange={this.handleImageChange} />
+              {
+                this.state.image_file_blob_or_url === null ?
+                  "" : 
+                  <div>
+                    <ImagePreview fileBlobOrURL={this.state.image_file_blob_or_url} />
+                    <HSLPlot fileBlobOrURL={this.state.image_file_blob_or_url} />
+                  </div>
+              }
             </div>
-        }
+          </div>
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
